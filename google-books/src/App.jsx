a11127711938ec03a39bbb8f9bbde3e8e7ba57b5
ChloeBallantyne <160100291+ChloeBallantyne@users.noreply.github.com>
2024-04-05 14:18:42 +1100

    header and search bar

---
+++ google-books/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header/Header";
+import SearchBar from "./Components/SearchBar/SearchBar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <form>
+        <SearchBar />
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ google-books/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SearchBar.module.scss";
+
+const SearchBar = () => {
+  return <input className={styles.bar} type="text" />;
+};
+
+export default SearchBar;
